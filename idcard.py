6c7e1bc24f599be1e92643ecb6a0158435ea19f5
Berlian Safri Prakoso <berliansafrip@gmail.com>
2023-03-04 22:31:46 +0700

    aman

--- idcard.py
+++ idcard.py
@@ -1,24 +1,44 @@
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 import os
-# Load the ID card design
-idcard = Image.open("desain_idcard.png")
 
-# Get the barcode filenames
-import os
+# Open Resource
 barcode_folder = "F:/KULIAH UNS/Lab/POSI/idcard/barcode"
-barcode_filenames = os.listdir(barcode_folder)
+idcard_image = "F:/KULIAH UNS/Lab/POSI/idcard/desain_idcard.png"
 
-# Loop through the barcode filenames
-for filename in barcode_filenames:
-    if filename.endswith(".png"):
-        # Load the barcode image
-        barcode = Image.open(os.path.join(barcode_folder, filename))
-        
-        # Resize the barcode image to fit on the ID card
-        barcode = barcode.resize((200, 100))
-        
-        # Paste the barcode onto the ID card
-        idcard.paste(barcode, (300, 300)) # Ganti koordinat x dan y sesuai dengan desain ID cardmu
-        
-        # Save the new ID card with barcode
-        idcard.save("idcard_"+filename)
+# Ukuran dan letak teks nama siswa pada idcard
+text_size = 50
+text_color = (255, 255, 255)  # warna teks (putih)
+text_position = (50, 50)  # koordinat letak teks (x, y)
+
+# Buat objek font untuk menulis teks
+font = ImageFont.truetype("Poppins-Medium.ttf", text_size)
+
+# Baca gambar idcard
+idcard = Image.open(idcard_image)
+
+# Loop untuk menambahkan barcode dan nama siswa pada idcard
+for barcode_file in os.listdir(barcode_folder):
+    # Ambil nama siswa dari nama file barcode
+    nama_siswa = os.path.splitext(barcode_file)[0]
+    
+    # Baca gambar barcode
+    barcode_path = os.path.join(barcode_folder, barcode_file)
+    barcode = Image.open(barcode_path)
+
+    # Resize gambar barcode agar sesuai dengan ukuran yang diinginkan
+    barcode = barcode.resize((200, 100))
+    
+    # Letakkan gambar barcode pada gambar idcard
+    idcard.paste(barcode, (100, 200))
+    
+    # Buat objek draw untuk menulis teks pada gambar idcard
+    draw = ImageDraw.Draw(idcard)
+    
+    # Tulis nama siswa pada gambar idcard
+    draw.text(text_position, nama_siswa, font=font, fill=text_color)
+    
+    # Simpan gambar idcard yang sudah ditambahkan barcode dan nama siswa
+    idcard.save(f"{nama_siswa}_idcard.png")
+    
+    # Hapus gambar barcode dari memori agar tidak menghabiskan memori
+    del barcode
